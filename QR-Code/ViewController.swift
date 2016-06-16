3b4635b3154bedc3ad72e3f84ee11a2f9bdfadd9
Satoshi Ogata <satosystems@gmail.com>
2016-06-16 20:38:35 +0900

    Initial Commit

---
+++ QR-Code/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  QR-Code
+//
+//  Created by Satoshi Ogata on 2016/06/16.
+//  Copyright © 2016年 App research and development. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
