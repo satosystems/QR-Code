761b717ab52774c4bfc422c22cc7cf3ead473460
Satoshi Ogata <satosystems@gmail.com>
2016-06-16 21:27:55 +0900

    remove storyboard for this project

--- QR-Code/AppDelegate.swift
+++ QR-Code/AppDelegate.swift
@@ -16,6 +16,14 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
         // Override point for customization after application launch.
+        window = UIWindow(frame: UIScreen.mainScreen().bounds)
+
+        let rootViewController = UIViewController()
+        rootViewController.view.backgroundColor = UIColor.blueColor()  // FIXME: for debug
+
+        window?.rootViewController = rootViewController
+        window?.makeKeyAndVisible()
+
         return true
     }
 
