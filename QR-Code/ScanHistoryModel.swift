7ac5827484438a432d3d1a51a2284b6fccb5e30e
Satoshi Ogata <satosystems@gmail.com>
2016-06-17 02:08:08 +0900

    add data model of scan histories

---
+++ QR-Code/ScanHistoryModel.swift
@@ -0,0 +1,41 @@
+//
+//  ScanHistoryModel.swift
+//  QR-Code
+//
+//  Created by Satoshi Ogata on 2016/06/17.
+//  Copyright © 2016年 App research and development. All rights reserved.
+//
+
+import Foundation
+
+class ScanHistoryModel {
+    private let key = "histories"
+    private static let singleton = ScanHistoryModel()
+    var histories: [String]?
+
+    static func getInstance() -> ScanHistoryModel {
+        return singleton
+    }
+
+    func addScanHistory(data: String) {
+        if (histories == nil) {
+            histories = loadScanHistories()
+        }
+        histories?.insert(data, atIndex: 0)
+        let userDefaults = NSUserDefaults.standardUserDefaults()
+        userDefaults.setObject(histories, forKey: key)
+        userDefaults.synchronize()
+    }
+
+    func loadScanHistories() -> [String] {
+        if (histories != nil) {
+            return histories!
+        }
+        let userDefaults = NSUserDefaults.standardUserDefaults()
+        histories = userDefaults.stringArrayForKey(key)
+        if (histories == nil) {
+            histories = []
+        }
+        return histories!
+    }
+}
