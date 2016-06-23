73a5b0da1ba8d65721c22667dfa9a1d3b1555ae1
Satoshi Ogata <satosystems@gmail.com>
2016-06-23 22:27:07 +0900

    add log when uncaught exception

--- src/AppDelegate.swift
+++ src/AppDelegate.swift
@@ -15,6 +15,11 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
 
     func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
+        NSSetUncaughtExceptionHandler { exception in
+            print("Exception:", exception);
+            print("Stack Trace:", exception.callStackSymbols);
+        }
+
         // Override point for customization after application launch.
         window = UIWindow(frame: UIScreen.mainScreen().bounds)
 
